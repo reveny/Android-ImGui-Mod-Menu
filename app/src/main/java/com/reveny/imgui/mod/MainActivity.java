d7a2bce0c295e60ac0c92bf7762d2cd9f6a28040
Reveny <113244907+reveny@users.noreply.github.com>
2023-09-01 11:35:13 +0200

    Added Test OpenGL View

--- app/src/main/java/com/reveny/imgui/mod/MainActivity.java
+++ app/src/main/java/com/reveny/imgui/mod/MainActivity.java
@@ -1,16 +1,52 @@
 package com.reveny.imgui.mod;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.reveny.imgui.mod.test.myGlSurfaceView;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.loadLibrary("Loader");
+        //Taken from https://github.com/JimSeker/opengl/blob/master/OpenGL30Cube/app/src/main/java/edu/cs4730/opengl30cube/MainActivity.java
+        if (detectOpenGLES30()) {
+            //so we know it a opengl 3.0 and use our extended GLsurfaceview.
+            setContentView(new myGlSurfaceView(this));
+        } else {
+            // This is where you could create an OpenGL ES 2.0 and/or 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            Log.e("OpenGL", "OpenGL ES 3.0 not supported on device.");
+            finish();
+        }
+
+        System.loadLibrary("ModMenu");
+    }
+
+    private boolean detectOpenGLES30() {
+        ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo info = am.getDeviceConfigurationInfo();
+        return (info.reqGlEsVersion >= 0x30000);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
     }
 }
--- app/src/main/jni/Main/Main.cpp
+++ app/src/main/jni/Main/Main.cpp
@@ -15,8 +15,6 @@ void DrawMenu() {
 
 void *thread(void *) {
     LOGI(OBFUSCATE("Main Thread Loaded: %d"), gettid());
-
-    //This will only work ingame and not in the test app
     initModMenu((void *)DrawMenu);
 
     //Hooks, Patches and Pointers here
@@ -38,5 +36,5 @@ void init() {
     pthread_create(&t, nullptr, thread, nullptr);
 
     //Don't leave any traces, remap the loader lib as well
-    RemapTools::RemapLibrary("libLoader.so");
+    //RemapTools::RemapLibrary("libLoader.so");
 }
