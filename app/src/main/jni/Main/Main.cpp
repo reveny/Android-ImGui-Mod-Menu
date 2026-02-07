6fc89c939d57eaec95f353f93782d6ce9d66f83e
Vladimir <131596433+Blitchcore@users.noreply.github.com>
2026-02-07 12:43:43 +0600

    Update Main.cpp

--- app/src/main/jni/Main/Main.cpp
+++ app/src/main/jni/Main/Main.cpp
@@ -16,7 +16,7 @@ void DrawMenu() {
 void *thread(void *) {
     LOGI(OBFUSCATE("Main Thread Loaded: %d"), gettid());
     initModMenu((void *)DrawMenu);
-    SetupInpuyHooks();
+    SetupInputHooks();
 
     //Hooks, Patches and Pointers here
     //Example:
